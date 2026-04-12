1dc0f87d7b466f02f63de4ca37b5e566f7ea852a
Rishi <rishi75503500@gmail.com>
2026-04-12 02:27:49 -0700

    Update API URL for production

--- src/config/api.ts
+++ src/config/api.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API = axios.create({ baseURL: "/api" });
+const API = axios.create({ baseURL: "https://qwikwebs-backend-production.up.railway.app/api" });
 
 API.interceptors.request.use((config) => {
   const token = localStorage.getItem("token");
